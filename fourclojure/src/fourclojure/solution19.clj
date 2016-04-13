cfc38869c3adcb6316051460587d26f26d5104c7
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-13 15:36:05 +0200

    Add sols to 19

--- fourclojure/src/fourclojure/solution19.clj
+++ fourclojure/src/fourclojure/solution19.clj
@@ -6,7 +6,20 @@
 
 (= (#(first(reverse %)) ["b" "c" "d"]) "d")
 
-;; --
+;; solution 2
+
+(defn sol2 [coll]
+  (.get coll(- (count coll) 1)))
+
+;; solution 3
+(defn sol3 [coll]
+  (nth coll (dec (count coll))))
+
+(= (sol3 [1 2 3 4 5]) 5)
+
+(= (sol3 '(5 4 3)) 3)
+
+(= (sol2 ["b" "c" "d"]) "d")
 
 
 
