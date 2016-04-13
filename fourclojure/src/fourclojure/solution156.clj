196831f32090cf2e3c517fd448274b5420b362bf
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-13 14:02:52 +0200

    Add sols to 156.

---
+++ fourclojure/src/fourclojure/solution156.clj
@@ -0,0 +1,32 @@
+(ns fourclojure.solution156)
+
+; (zipmap  [:a :b :c] (repeat 0))
+
+(= (#(zipmap %2 (repeat %1)) 0 [:a :b :c]) {:a 0 :b 0 :c 0})
+
+(= (#(zipmap %2 (repeat %1)) "x" [1 2 3]) {1 "x" 2 "x" 3 "x"})
+
+(= (#(zipmap %2 (repeat %1)) [:a :b] [:foo :bar]) {:foo [:a :b] :bar [:a :b]})
+
+
+;; solution 2
+
+(defn sol2 [v coll]
+  (into {} (for [x coll] {x v})))
+
+;; solution 3
+
+(defn sol3 [v coll]
+  (into {} (map vector coll (repeat v))))
+
+;; solution 4
+
+(defn sol4 [v coll]
+  (apply assoc {} (interleave coll (repeat v))))
+
+(= (sol4 0 [:a :b :c]) {:a 0 :b 0 :c 0})
+
+(= (sol4 "x" [1 2 3]) {1 "x" 2 "x" 3 "x"})
+
+(= (sol4 [:a :b] [:foo :bar]) {:foo [:a :b] :bar [:a :b]})
+
