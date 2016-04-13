ae526a2a3e5b340301284564bc03ac05ec911f8d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-13 22:57:39 +0200

    Sol 21.

---
+++ fourclojure/src/fourclojure/solution21.clj
@@ -0,0 +1,38 @@
+(ns fourclojure.solution21)
+
+;; solution 1
+(defn nth-elt-1 [coll n]
+  ((vec coll) n))
+
+;; solution 2
+(defn nth-elt-2 [coll n]
+   (first (drop n coll)))
+
+;; solution 3
+(defn nth-elt-3 [coll n]
+  (if (zero? n)
+    (first coll)
+    (recur (rest coll) (- n 1))))
+
+;; solution 4
+(defn nth-elt-4 [coll n]
+  (loop [s coll i n]
+   (if (= i 0) (first s)
+       (recur (next s) (dec i)))))
+
+;; solution 5
+(defn nth-elt-5 [coll n]
+  (first (nthrest coll n)))
+
+;; solution 6
+(defn nth-elt-6 [coll n]
+  (last (take (+ n 1) coll)))
+
+
+(= (nth-elt-6 '(4 5 6 7) 2) 6)
+
+(= (nth-elt-6 [:a :b :c] 0) :a)
+
+(= (nth-elt-1 [1 2 3 4] 1) 2)
+
+(= (nth-elt-1 '([1 2] [3 4] [5 6]) 2) [5 6])
