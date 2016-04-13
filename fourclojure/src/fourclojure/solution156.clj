2adf743f9727753c36bc4730f290903d4372aa99
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-13 14:05:14 +0200

    Add another sol to #156.

--- fourclojure/src/fourclojure/solution156.clj
+++ fourclojure/src/fourclojure/solution156.clj
@@ -10,21 +10,23 @@
 
 
 ;; solution 2
-
 (defn sol2 [v coll]
   (into {} (for [x coll] {x v})))
 
 ;; solution 3
-
 (defn sol3 [v coll]
   (into {} (map vector coll (repeat v))))
 
 ;; solution 4
-
 (defn sol4 [v coll]
   (apply assoc {} (interleave coll (repeat v))))
 
-(= (sol4 0 [:a :b :c]) {:a 0 :b 0 :c 0})
+;; solution 5
+(defn sol5 [v coll]
+  (zipmap coll (repeat (count coll) v)))
+
+
+(= (sol5 0 [:a :b :c]) {:a 0 :b 0 :c 0})
 
 (= (sol4 "x" [1 2 3]) {1 "x" 2 "x" 3 "x"})
 
