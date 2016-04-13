79da2803c91008f4586dc8813f4a7098456bd3a8
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-13 11:33:45 +0200

    Sol 134

--- fourclojure/src/fourclojure/solution134.clj
+++ fourclojure/src/fourclojure/solution134.clj
@@ -1,16 +1,12 @@
 (ns fourclojure.solution134)
 
-(true? ((fn [k m]
+(defn contains-nil-val? [k m]
           (and (contains? m k)
                (nil? (get m k))))
+
+(true? (contains-nil-val?
         :a {:a nil :b 2}))
 
-(false? ((fn [k m]
-           (and (contains? m k)
-                (nil? (get m k))))
-         :b {:a nil :b 2}))
+(false? (contains-nil-val? :b {:a nil :b 2}))
 
-(false? ((fn [k m]
-           (and (contains? m k)
-                (nil? (get m k))))
-         :c {:a nil :b 2}))
+(false? (contains-nil-val? :c {:a nil :b 2}))
