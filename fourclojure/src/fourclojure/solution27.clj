163c6943e03692998ea98a71e9bca1c1397d52ab
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-23 02:40:51 +0200

    Add sol to pb 27

---
+++ fourclojure/src/fourclojure/solution27.clj
@@ -0,0 +1,10 @@
+(ns fourclojure.solution27)
+
+(defn sol1 [coll]
+  (= coll (reverse coll)))
+
+(false? (sol1 '(1 2 3 4 5)))
+(true? (sol1 "racecar"))
+(true? (sol1 [:foo :bar :foo]))
+(true? (sol1 '(1 1 3 3 1 1)))
+(false? (sol1 '(:a :b :c)))
