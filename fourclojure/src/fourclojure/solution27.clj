fa6184ad73d5aed80b4e5294efe2afd72784d8a6
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-23 03:20:06 +0200

    Add sol to pb 27

--- fourclojure/src/fourclojure/solution27.clj
+++ fourclojure/src/fourclojure/solution27.clj
@@ -1,10 +1,17 @@
 (ns fourclojure.solution27)
 
 (defn sol1 [coll]
-  (= (seq coll) (reverse (seq coll))))
+  (= (seq coll) (reverse coll)))
 
-(false? (sol1 '(1 2 3 4 5)))
-(true? (sol1 "racecar"))
+(defn sol2 [coll]
+  (= (apply str (reverse coll)) (apply str coll)))
+
+;(defn sol2 [coll]
+;  (let [re (split-at (rem (count coll) 2) coll)]
+;   (= (first res) (rest res))))
+
+(false? (sol2 '(1 2 3 4 5)))
+(true? (sol2 "racecar"))
 (true? (sol1 [:foo :bar :foo]))
 (true? (sol1 '(1 1 3 3 1 1)))
 (false? (sol1 '(:a :b :c)))
