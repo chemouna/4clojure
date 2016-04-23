9979f2256c667cbfa1b7fcef81e9600f61e97376
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-23 02:31:25 +0200

    Add sol to pb 23

--- fourclojure/src/fourclojure/solution23.clj
+++ fourclojure/src/fourclojure/solution23.clj
@@ -6,7 +6,13 @@
 (defn sol2 [coll]
   (reduce #(conj %1 %2) '() coll))
 
-(= (sol2 [1 2 3 4 5]) [5 4 3 2 1])
-(= (sol2 (sorted-set 5 7 2 7)) '(7 5 2))
-(= (sol2 [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
+(defn sol3 [coll]
+  (apply conj () coll))
+
+(defn sol4 [coll]
+  (into () coll))
+
+(= (sol4 [1 2 3 4 5]) [5 4 3 2 1])
+(= (sol4 (sorted-set 5 7 2 7)) '(7 5 2))
+(= (sol4 [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
 
